914883298c08a222a0f110d8cd20a6e848316222
EXUBERANT EDWARD <s4106882@student.rmit.edu.au>
2025-04-09 14:52:42 +1000

    Combined UI and scraping.

--- Movie Scraper.py
+++ Movie Scraper.py
@@ -1,35 +1,71 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit
+from PyQt5.QtCore import Qt
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-import os
-import External
+import sys
 
-def scrape_movies():
+class MediaServerApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        # Set up window
+        self.setWindowTitle("Rudy's Awesome Media")
+        self.setGeometry(100, 100, 400, 400)
+        
+        layout = QVBoxLayout()
+        
+        self.label = QLabel("Welcome to my server!")
+        layout.addWidget(self.label)
+        
+        self.input_box = QLineEdit(self)
+        self.input_box.setPlaceholderText("Type something here...")
+        layout.addWidget(self.input_box)
+        
+        self.download_button = QPushButton("Search")
+        self.download_button.clicked.connect(self.on_download_clicked)
+        layout.addWidget(self.download_button)
+        
+        self.setLayout(layout)
+        
+    def on_download_clicked(self):
+        search_text = self.input_box.text()
+        if search_text:
+            self.label.setText("Searching...")
+            titles = scrape_movies(search_text)
+            if titles:
+                self.label.setText("\n".join(titles))
+            else:
+                self.label.setText("No results found.")
+        else:
+            self.label.setText("Please enter a search term.")
+        
+def scrape_movies(search_text):
     options = webdriver.ChromeOptions()
     options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
     options.add_argument('--headless')
     options.add_argument('--disable-images')
     options.add_argument('--disable-extensions')
     driver = webdriver.Chrome(options=options)
-    print(External.EXTERNAL_LINK)
-    search_text = str(input())
-    driver.get(f'{External.EXTERNAL_LINK}{search_text}') #https://flickystream.com/search?q=
+    
+    driver.get(f'https://flickystream.com/search?q={search_text}')
     driver.maximize_window()
     
     WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'h3.text-white.font-medium.line-clamp-1.text-balance'))
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'h3.text-white.font-medium.line-clamp-1.text-balance'))
     )
     
     movies = driver.find_elements(By.CSS_SELECTOR, 'h3.text-white.font-medium.line-clamp-1.text-balance')
     movies_titles = [movie.text for movie in movies]
     
-    input("Press enter to close the browser")
-    
     driver.quit()
     return movies_titles
 
-titles = scrape_movies()
-print(titles)
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MediaServerApp()
+    window.show()
+    sys.exit(app.exec_())
--- UI.py
+++ UI.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
+'''
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit
 from PyQt5.QtCore import Qt
 
 class MediaServerApp(QWidget):
@@ -14,17 +15,26 @@ class MediaServerApp(QWidget):
         self.label = QLabel("Welcome to my server!")
         layout.addWidget(self.label)
         
-        self.download_button = QPushButton("Download Movie")
+        self.input_box = QLineEdit(self)
+        self.input_box.setPlaceholderText("Type something here...")
+        layout.addWidget(self.input_box)
+        
+        self.download_button = QPushButton("Search")
         self.download_button.clicked.connect(self.on_download_clicked)
         layout.addWidget(self.download_button)
         
         self.setLayout(layout)
         
     def on_download_clicked(self):
-        self.download_button.setText("Download Started!")
+        self.download_button.setText("Searching...")
+        output = self.input_box.text()
+        return output
+
+        
         
 if __name__ == '__main__':
     app = QApplication([])
     window = MediaServerApp()
     window.show()
     app.exec_()
+'''
