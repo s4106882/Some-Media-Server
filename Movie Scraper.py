18817ef8a63b10605065be405c4313321240885f
EXUBERANT EDWARD <s4106882@student.rmit.edu.au>
2025-04-09 12:58:26 +1000

    Update Movie Scraper.py

--- Movie Scraper.py
+++ Movie Scraper.py
@@ -7,6 +7,8 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 
 def scrape_movies():
+    search_text = os.getenv('SEARCH_TEXT', 'Minecraft')
+    
     options = webdriver.ChromeOptions()
     options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
     options.add_argument('--headless')
